e91409039ce2b2ae45903805d45d43fd7602118a
Yann <yann.demaux86@gmail.com>
2024-02-28 09:57:27 +0100

    new filterUsers méthodes

--- src/components/MyContacts.js
+++ src/components/MyContacts.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import SearchInput from "./SearchInput";
-import useUpdateTitle from "../hooks/useUpdateTitle";
+// import useUpdateTitle from "../hooks/useUpdateTitle";
 import TableUsers from "./TableUsers";
 
 const MyContacts = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [search, setSearch] = useState("");
+  const [resultSearch, setResultSearch] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -18,8 +19,28 @@ const MyContacts = () => {
       .catch((error) => console.log(error.message));
   }, []);
 
+  const filterUsers = () => {
+    const foundUsers = users.filter((users) => {
+      return Object.values(users)
+        .join("")
+        .toLowerCase()
+        .includes(search.toLowerCase());
+    });
+
+    setResultSearch(foundUsers);
+  };
+
+  useEffect(() => {
+    if (search !== "") {
+      // Filter
+      filterUsers();
+    } else {
+      setResultSearch([]);
+    }
+  }, [search]);
+
   // Custom Hook
-  useUpdateTitle(search);
+  // useUpdateTitle(search);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -37,7 +58,14 @@ const MyContacts = () => {
         <SearchInput searchStr={search} searchHanlder={handleChange} />
       )}
 
-      {<TableUsers dataArray={users} />}
+      {resultSearch.length === 0 && search !== "" ? (
+        msgDisplay("Pas de résultat !", "red")
+      ) : search === "" ? (
+        msgDisplay("Veuillez effectuer une recherche !", "green")
+      ) : (
+        //search === "" ? null
+        <TableUsers dataArray={resultSearch} />
+      )}
     </>
   );
 };
