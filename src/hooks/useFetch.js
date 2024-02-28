e457158c9c6e6e1c4c4868116142534eb79a63aa
Yann <yann.demaux86@gmail.com>
2024-02-28 18:19:36 +0100

    add useDebugValue on custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 const useFetch = (fetchUrl) => {
+  // State
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  // Array
+  // cibler un user dans le fetch de data via la méthode useDebugValue
+  // useDebugValue(["User 6", data[5]]);
+  // useDebugValue(JSON.stringify(data));
+
+  // Si je suis en mode dev et que React dev tools est ouvert pour inspecter le custom hook!
+  useDebugValue(data, (val) => JSON.stringify(val));
 
   // Fetch
   useEffect(() => {
